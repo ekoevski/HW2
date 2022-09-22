5d277f0792aabedda3fe240e71c41a54cddd1767
ekoevski <ekoevski@gmail.com>
2022-09-22 16:15:41 -0600

    from onedrive

--- src/edu/ucdenver/library/EBook.java
+++ src/edu/ucdenver/library/EBook.java
@@ -1,2 +1,43 @@
-package edu.ucdenver.library;public class EBook {
-}
+package edu.ucdenver.library;
+import java.time.LocalDate;
+
+
+
+    public class EBook extends Book {
+
+
+        //Declaration
+        //================
+        private int numPages;
+        private int wordsPerPage;
+
+        //Constructor
+        //================
+        public EBook(String subTitle, LocalDate subdate, String[] subOtherTitles, Author subauthor, String sibisbn, int pageNum, int argWordsPerPage) {
+            title = subTitle;
+            publicationDate = subdate;
+            otherTitles = subOtherTitles;
+            isbn = sibisbn;
+            numPages = pageNum;
+            wordsPerPage = argWordsPerPage;
+            bookAuthor = subauthor;
+
+        }
+
+
+
+        //Methods
+
+        public int getNumPages() {
+            return numPages;
+        }
+
+
+        public String toString(){
+            String out = "Electronic-Book: " + title + " isbn: isbn(" + isbn + ") , published on " + publicationDate + " authored by " +
+                    bookAuthor.getName() + "(Author) with " + numPages + " pages and " + otherTitles.length + " other titles.";
+            return out;
+        }
+
+
+    }
--- src/edu/ucdenver/library/PrintedBook.java
+++ src/edu/ucdenver/library/PrintedBook.java
@@ -1,2 +1,37 @@
-package edu.ucdenver.library;public class PrintedBook {
+package edu.ucdenver.library;
+import java.time.LocalDate;
+
+
+
+public class PrintedBook extends Book {
+
+    //Declaration
+    //================
+    private int numPages;
+
+
+    //Constructor
+    //================
+    public PrintedBook(String subTitle, LocalDate subdate, String[] subOtherTitles, Author subauthor, String sibisbn, int pageNum) {
+        title = subTitle;
+        publicationDate = subdate;
+        otherTitles = subOtherTitles;
+        isbn = sibisbn;
+        bookAuthor = subauthor;
+        numPages = pageNum;
+    }
+
+    //Methods
+
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public String toString(){
+        String out = "Printed-Book: " + title + " isbn: isbn(" + isbn + ") , published on " + publicationDate + " authored by " +
+                bookAuthor.getName() + "(Author) with " + numPages + " pages and " + otherTitles.length + " other titles.";
+        return out;
+    }
+
 }
